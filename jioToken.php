4b7b12d16c9c25d8773f40ee08afeb38abae8208
allscripts <37670589+allscriptz@users.noreply.github.com>
2019-02-12 10:45:51 +0530

    Add files via upload

---
+++ jioToken.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+
+Use it only for personal use. Mail to the address below for support, if necessary.
+Contributed by: allscripts@protonmail.com
+
+*/
+
+echo exec("java token.runner 9XM 2>&1");
+
+?>
